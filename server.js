747d1a266a72b4d85bdb587a2690e15518aca4ec
r-kazama <r-kazama@sstn.jp>
2018-07-22 15:32:53 +0900

    add イニット処理

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const line = require('@line/bot-sdk');
+const utils = require('./utils');
 const { config, fbConfig } = require('./config')
 const PORT = process.env.PORT || 3000;
 
@@ -23,7 +24,23 @@ function handleEvent(event) {
       text: `友だち登録ありがとうございます٩( ᐛ )و\nTEDUNAは，集合住宅居住者向けコミュニケーションアプリです\n\n郵便番号と建物名を以下のように入力してください\n例：100-8111/皇居`
     });
 
-  }else if (event.type == 'message' || event.message.type == 'text') {
+  } else if (event.type == 'message' || event.message.type == 'text') {
+    let msg = event.message.text;
+    let conf = msg.split("/");
+    if (msg.match(/^[0-9]{3}-?[0-9]{4}\/.+/)) {
+      var res = utils.isExistDatabaseAsyc(conf[0], conf[1]);
+      if (res.exist) {
+        utils.appendUser(event.source.userId, res.key);
+      } else {
+        utils.createBuilding(event.source.userId, conf[0], conf[1]);
+      }
+
+      client.replyMessage(event.replyToken, {
+        type: "text",
+        text: conf[0] + "\n" + conf[1] + "\n" + "を登録しました"
+      });
+    }
+
     return client.replyMessage(event.replyToken, {
       type: 'text',
       text: event.message.text //実際に返信の言葉を入れる箇所
