8bf9b6a1f647dd1b6d801d9bbc07308287a670dc
白田光 <shirata@ShirataHikarunoMacBook-Air.local>
2018-07-22 12:43:33 +0900

    add follow event

--- server.js
+++ server.js
@@ -17,14 +17,22 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 const client = new line.Client(config);
 
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
+  if (event.type === 'follow') {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: `友だち登録ありがとうございます(happy)
+      TEDUNAは，集合住宅居住者向けコミュニケーションアプリです
+      
+      まずは，郵便番号と建物名を以下のように入力してください(star)
+      例：100-8111/皇居`
+    });
 
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text //実際に返信の言葉を入れる箇所
-  });
+  }else if (event.type !== 'message' || event.message.type !== 'text') {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.message.text //実際に返信の言葉を入れる箇所
+    });
+  }
 }
 
 app.listen(PORT);
